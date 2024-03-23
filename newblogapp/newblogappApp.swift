cee6265a368c1360ea5c1eef62a0fd3eb1071ddd
softdevomar <omorfarukqqq@gmail.com>
2024-03-23 04:45:04 -0700

    Initial Commit

---
+++ newblogapp/newblogappApp.swift
@@ -0,0 +1,17 @@
+//
+//  newblogappApp.swift
+//  newblogapp
+//
+//  Created by Md omor on 23/3/24.
+//
+
+import SwiftUI
+
+@main
+struct newblogappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
